899ccacbe1387694d79dd138f9598dc739af9bba
Guillem Santamaria Aguilar guillem.santamaria@uni.lu <gsantamaria@access1.iris-cluster.uni.lux>
2024-07-11 20:16:41 +0200

    Add script for merging LINCS dataset with previous compilation. Add pipeline for preproc of this dataset.

---
+++ scripts/lincs_obtain/merge_lincs_rest.R
@@ -0,0 +1,102 @@
+################################################################################
+# Brain clock: this script gets as input the lincs expression data and the     #
+# quantNorm(log2(counts + 1)) integrated dataset (Synapse+TBI+111_perFiles)    #
+# and merges them in a single dataset, which will be the input of the          #
+# processing pipeline (sva etc).                                               #
+################################################################################
+
+lincsFile <- "/mnt/lscratch/users/gsantamaria/test_large_files/NPC/parsed_mats/lincs_NPC_concat_expMat.csv"
+integFile <- "/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_counts_wTBI_wPert111_log2_quantNorm_preproc.csv"
+
+metDatInteg <- "/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_metDat_wTBI_wPert111.csv"
+metDatLincs <- "/mnt/lscratch/users/gsantamaria/test_large_files/NPC/parsed_mats/lincs_NPC_concat_metDat.csv"
+
+outDir <- "/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/"
+outName <- gsub(".csv", "", basename(integFile))
+outPath <- sprintf("%s%s_wLINCS.csv", outDir, outName)
+
+outNameMetDat <- gsub(".csv", "", basename(metDatInteg))
+outPathMetDat <- sprintf("%s%s_wLINCS.csv", outDir, outNameMetDat)
+# Functions
+################################################################################
+
+# read.csv but faster
+readCsvFst <- function(pth, header = T){
+        df <- data.frame(data.table::fread(pth, header = header))
+        rownames(df) <- df$V1
+        df <- df[, colnames(df) != "V1"]
+        return(df)
+}
+
+# write.csv, but faster
+writeCsvFst <- function(df, file, rowNames = T, colNames = T){
+        if(rowNames){
+                rn <- rownames(df)
+                df <- data.table::data.table(df)
+                df[, V1 := rn]
+                data.table::setcolorder(df, c("V1", setdiff(names(df), "V1")))
+        }else{
+                df <- data.table::data.table(df)
+        }
+        data.table::fwrite(df, file, col.names = colNames)
+}
+
+# Load data
+################################################################################
+lincs <- readCsvFst(lincsFile)
+integ <- readCsvFst(integFile)
+lincs <- data.frame(t(lincs))
+
+metDat_lincs <- readCsvFst(metDatLincs)
+metDat_integ <- readCsvFst(metDatInteg)
+
+# Merge expression files
+################################################################################
+
+commGenes <- intersect(colnames(lincs), colnames(integ))
+
+lincsInteg <- rbind.data.frame(lincs[, commGenes],
+                               integ[, commGenes])
+
+# Merge metadata files
+################################################################################
+metDat_lincs$diagn_4BrainClck <- rep("perturbations",
+                                     nrow(metDat_lincs))
+
+metDat_lincs$perturbation <- paste(metDat_lincs$pertname,
+                                   metDat_lincs$dose,
+                                   metDat_lincs$timepoint,
+                                   sep = "_")
+
+metDat_lincs$substudy <- rep("LINCS", nrow(metDat_lincs))
+metDat_lincs$RIN <- rep(max(metDat_integ$RIN[!is.na(metDat_integ$RIN)]),
+                        nrow(metDat_lincs))
+
+metDat_lincs <- metDat_lincs[, c("specimenID", "diagn_4BrainClck", "perturbation", "group", "RIN")]
+colnames(metDat_lincs) <- gsub("group", "exper_group", colnames(metDat_lincs))
+
+cols2Add_metDatLincs <- colnames(metDat_integ)[!colnames(metDat_integ) %in% colnames(metDat_lincs)]
+
+df2AddLincs <- data.frame(matrix(nrow = nrow(metDat_lincs),
+                                 ncol = length(cols2Add_metDatLincs),
+                                 dimnames = list(rownames(metDat_lincs),
+                                                 cols2Add_metDatLincs)))
+
+metDat_lincs <- cbind.data.frame(metDat_lincs, df2AddLincs)
+
+metDat_lincsInteg <- rbind.data.frame(metDat_integ,
+                                      metDat_lincs[, colnames(metDat_integ)])
+
+medianAge <- median(metDat_lincsInteg$ageDeath[!is.na(metDat_lincsInteg$ageDeath)])
+metDat_lincsInteg$ageDeath[is.na(metDat_lincsInteg$ageDeath)] <- medianAge
+# Save expression and metadata datasets
+################################################################################
+writeCsvFst(lincsInteg, file = outPath)
+print(sprintf("%s saved at %s.",
+              basename(outPath),
+              dirname(outPath)))
+
+writeCsvFst(metDat_lincsInteg, file = outPathMetDat)
+print(sprintf("%s saved at %s.",
+              basename(outPathMetDat),
+              dirname(outPathMetDat)))
---
+++ scripts/preprocessing/launch_preproc_pipe_intDF_wLincsN111.sh
@@ -0,0 +1,183 @@
+#!/bin/bash -l
+#Usage # sbatch [this script]
+#Name of the job
+#SBATCH --job-name=preproc
+#SBATCH -N 1
+#SBATCH --mail-user=guillem.santamaria@uni.lu
+#SBATCH --mail-type=begin,end,fail
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=300GB
+#SBATCH -c 8
+#SBATCH --time=00-02:00:00
+#Define sdout path
+#SBATCH --output=/home/users/gsantamaria/projects/brain_clock/scripts/output_preproc_pipe_lincs.txt
+#Define sderr path
+#SBATCH --error=/home/users/gsantamaria/projects/brain_clock/scripts/error_preproc_pipe_lincs.txt
+#Define the queue (Quality Of Service) to which the task shall be submitted to
+#SBATCH -p bigmem
+#SBATCH --qos=normal
+
+conda activate r-4.3.1
+
+# This version of the preproc pipeline uses as input the quantNorm(log(counts + 1)) of the integrated dataset,
+# consisting of synapse.org datasets, TBI 111 compilation of GEO obtained by Sascha and Javier for brain specific
+# cell types and NPC samples of LINCS1000 (level 3).
+
+# Variables for the pipeline
+########################################################################################################################
+
+input="/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_counts_wTBI_wPert111_log2_quantNorm_preproc_wLINCS.csv"
+metDat="/home/users/gsantamaria/projects/brain_clock/results/preprocessing/combined_metDat_wTBI.csv"
+propZerosRem=0.8
+nPCs=20
+tiss2rem="cerebellum,cerebellar hemisphere"
+outTag="noCerebell"
+rinFilt=6
+outDir="/home/users/gsantamaria/projects/brain_clock/results/preprocessing/wTPI_lincs/"
+
+# Preprocess the TPMs file
+Rscript tpms_preproc.R $input --metDat $metDat --propZerosRem $propZerosRem --rinFilt $rinFilt --outDir $outDir
+
+logSuff="_preproc_log10.csv"
+
+inPCALog=$(basename "$input")
+inPCALog=$(echo "$inPCALog" | sed 's/.csv$//')
+inPCALog="${outDir}${inPCALog}${logSuff}"
+
+# Do PCA of preprocessed file and plot it.
+Rscript bigPCA_exe.R $inPCALog --nPCs $nPCs --stand --outDir $outDir
+pcaLogFile=$(echo "$inPCALog" | sed 's/.csv$//')
+pcaLogFile="${pcaLogFile}_pca.rds"
+Rscript plotBigPCA_exe.R $pcaLogFile --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+
+# Remove cerebellum samples and run PCA again
+Rscript tpms_tissOutFilt.R $inPCALog --metDat $metDat --tiss2rem "$tiss2rem" --outTag $outTag --outDir $outDir
+
+noCerebFile=$(echo "$inPCALog" | sed 's/.csv$//')
+noCerebFile="${noCerebFile}_${outTag}.csv"
+
+Rscript bigPCA_exe.R $noCerebFile --nPCs $nPCs --stand --outDir $outDir
+pcaNoCerebFile=$(echo "$noCerebFile" | sed 's/.csv$//')
+pcaNoCerebFile="${pcaNoCerebFile}_pca.rds"
+Rscript plotBigPCA_exe.R $pcaNoCerebFile --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+
+# Create objects necessary for batch removal steps
+Rscript create_batchRemObjkts.R $noCerebFile --metDat $metDat --outDir $outDir
+
+# Run SVA with no covariates and with covariates, and ComBat
+mod_onlyAge=$(echo "$noCerebFile" | sed 's/.csv$//')
+mod_onlyAge="${mod_onlyAge}_svaMod_onlyAge.rds"
+mod0_onlyAge=$(echo "$noCerebFile" | sed 's/.csv$//')
+mod0_onlyAge="${mod0_onlyAge}_svaMod0_onlyAge.rds"
+
+mod_all=$(echo "$noCerebFile" | sed 's/.csv$//')
+mod_all="${mod_all}_svaMod_all.rds"
+mod0_all=$(echo "$noCerebFile" | sed 's/.csv$//')
+mod0_all="${mod0_all}_svaMod0_all.rds"
+
+batch=$(echo "$noCerebFile" | sed 's/.csv$//')
+batch="${batch}_batches.rds"
+
+modCombat=$(echo "$noCerebFile" | sed 's/.csv$//')
+modCombat="${modCombat}_combatMod.rds"
+
+Rscript sva_exe.R $noCerebFile --mod $mod_onlyAge --mod0 $mod0_onlyAge --saveSVrem --outDir $outDir &
+Rscript sva_exe.R $noCerebFile --mod $mod_all --mod0 $mod0_all --saveSVrem --outDir $outDir &
+Rscript combat_exe.R $noCerebFile --batch $batch --combatMod $modCombat --outDir $outDir &
+wait
+
+svaAdj_onlyAge=$(echo "$noCerebFile" | sed 's/.csv$//')
+svaAdj_onlyAge="${svaAdj_onlyAge}_onlyAge_svaAdj.csv"
+
+svaAdj_all=$(echo "$noCerebFile" | sed 's/.csv$//')
+svaAdj_all="${svaAdj_all}_all_svaAdj.csv"
+
+combatAdj=$(echo "$noCerebFile" | sed 's/.csv$//')
+combatAdj="${combatAdj}_combat.csv"
+
+# Run PCAs of the results
+Rscript bigPCA_exe.R $svaAdj_onlyAge --nPCs $nPCs --stand --outDir $outDir &
+#PID_sva_onlyAge=$!
+
+Rscript bigPCA_exe.R $svaAdj_all --nPCs $nPCs --stand --outDir $outDir &
+#PID_sva_all=$!
+
+Rscript bigPCA_exe.R $combatAdj --nPCs $nPCs --stand --outDir $outDir &
+#PID_combat=$!
+wait
+
+# Run SVA of the three batch effect removal approaches
+# and plot surrogate variables to see if batch effect is 
+# still present (without saving df with regressed-out SVs)
+Rscript sva_exe.R $svaAdj_onlyAge --mod $mod_onlyAge --mod0 $mod0_onlyAge --outDir $outDir &
+Rscript sva_exe.R $svaAdj_all --mod $mod_onlyAge --mod0 $mod0_onlyAge --outDir $outDir &
+Rscript sva_exe.R $combatAdj --mod $mod_onlyAge --mod0 $mod0_onlyAge --outDir $outDir &
+wait
+
+sva_after_sva_onlyAge=$(echo "$svaAdj_onlyAge" | sed 's/.csv$//')
+sva_after_sva_onlyAge="${sva_after_sva_onlyAge}_onlyAge_svobj.rds"
+Rscript sva_plot.R $svaAdj_onlyAge --sva $sva_after_sva_onlyAge --metDat $metDat --outDir $outDir
+
+sva_after_sva_all=$(echo "$svaAdj_all" | sed 's/.csv$//')
+sva_after_sva_all="${sva_after_sva_all}_onlyAge_svobj.rds"
+Rscript sva_plot.R $svaAdj_all --sva $sva_after_sva_all --metDat $metDat --outDir $outDir
+
+sva_after_combat=$(echo "$combatAdj" | sed 's/.csv$//')
+sva_after_combat="${sva_after_combat}_onlyAge_svobj.rds"
+Rscript sva_plot.R $combatAdj --sva $sva_after_combat --metDat $metDat --outDir $outDir
+
+# Plot PCAs
+pcaSVAOnlyAge=$(echo "$svaAdj_onlyAge" | sed 's/.csv$//')
+pcaSVAOnlyAge="${pcaSVAOnlyAge}_pca.rds"
+
+pcaSVAAll=$(echo "$svaAdj_all" | sed 's/.csv$//')
+pcaSVAAll="${pcaSVAAll}_pca.rds"
+
+pcaCombat=$(echo "$combatAdj" | sed 's/.csv$//')
+pcaCombat="${pcaCombat}_pca.rds"
+
+#(wait $PID_sva_onlyAge && Rscript plotBigPCA_exe.R $pcaSVAOnlyAge --metDat $metDat --x PC1 --y PC2 --outDir $outDir)
+#(wait $PID_sva_all && Rscript plotBigPCA_exe.R $pcaSVAAll --metDat $metDat --x PC1 --y PC2 --outDir $outDir)
+#(wait $PID_combat && Rscript plotBigPCA_exe.R $pcaCombat --metDat $metDat --x PC1 --y PC2 --outDir $outDir)
+Rscript plotBigPCA_exe.R $pcaSVAOnlyAge --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+Rscript plotBigPCA_exe.R $pcaSVAAll --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+Rscript plotBigPCA_exe.R $pcaCombat --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+
+# Remove effect of age and PMI from the ComBat dataset, as it is the only one that is properly
+# removing effect from substudy
+Rscript remAgePMI_exe.R $combatAdj --metDat $metDat --outDir $outDir
+
+# Run PCA of the result
+combatAgePMIAdj=$(echo "$combatAdj" | sed 's/.csv$//')
+combatAgePMIAdj="${combatAgePMIAdj}_pmiAgeAdj.csv"
+Rscript bigPCA_exe.R $combatAgePMIAdj --nPCs $nPCs --stand --outDir $outDir
+
+pcaCombatAgePMIAdj=$(echo "$combatAgePMIAdj" | sed 's/.csv$//')
+pcaCombatAgePMIAdj="${pcaCombatAgePMIAdj}_pca.rds"
+Rscript plotBigPCA_exe.R $pcaCombatAgePMIAdj --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+
+# Run SVA again (only giving age info) to assess if there are any remaining batch effects
+Rscript sva_exe.R $combatAgePMIAdj --mod $mod_onlyAge --mod0 $mod0_onlyAge --saveSVrem --outDir $outDir
+svaFile=$(echo "$combatAgePMIAdj" | sed 's/.csv$//')
+svaFile="${svaFile}_onlyAge_svobj.rds"
+Rscript sva_plot.R $combatAgePMIAdj --sva $svaFile --metDat $metDat --outDir $outDir
+
+# Run a PCA of the resulting dataset with the SVA regressed out.
+svaAdj=$(echo "$combatAgePMIAdj" | sed 's/.csv$//')
+svaAdj="${svaAdj}_onlyAge_svaAdj.csv"
+
+Rscript bigPCA_exe.R $svaAdj --nPCs $nPCs --stand --outDir $outDir
+
+pcaSvaAdj=$(echo "$svaAdj" | sed 's/.csv$//')
+pcaSvaAdj="${pcaSvaAdj}_pca.rds"
+
+Rscript plotBigPCA_exe.R $pcaSvaAdj --metDat $metDat --x PC1 --y PC2 --outDir $outDir
+
+# Run SVA again on this dataset (without saving the dataset with SVs regressed out)
+# to assess if the batch effect has been completely removed.
+Rscript sva_exe.R $svaAdj --mod $mod_onlyAge --mod0 $mod0_onlyAge --outDir $outDir
+svaFileFinal=$(echo "$svaAdj" | sed 's/.csv$//')
+svaFileFinal="${svaFileFinal}_onlyAge_svobj.rds"
+Rscript sva_plot.R $svaAdj --sva $svaFileFinal --metDat $metDat --outDir $outDir
+
+conda deactivate
