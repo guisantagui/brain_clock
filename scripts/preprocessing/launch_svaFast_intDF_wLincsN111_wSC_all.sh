ad5858b3d7721635dbca8a7b1dc06d16e02e023a
Guillem Santamaria Aguilar guillem.santamaria@uni.lu <gsantamaria@access1.iris-cluster.uni.lux>
2024-08-07 18:01:25 +0200

    Add scripts for including single cell samples

--- scripts/preprocessing/launch_svaFast_intDF_wLincsN111_wSC_all.sh
+++ scripts/preprocessing/launch_svaFast_intDF_wLincsN111_wSC_all.sh
@@ -27,15 +27,15 @@ conda activate r-4.3.1
 # Variables for the pipeline
 ########################################################################################################################
 
-input="/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_counts_wTBI_wPert111_log2_quantNorm_preproc_wLINCS.csv"
-metDat="/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_metDat_wTBI_wPert111_wLINCS.csv"
+input="/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_counts_wTBI_wPert111_wSC_log2_quantNorm_preproc_wLINCS.csv"
+metDat="/home/users/gsantamaria/projects/brain_clock/data/int_database_w111/combined_metDat_wTBI_wPert111_wSC_wLINCS.csv"
 filtDF="none" # "none" for not filtering
 #propZerosRem=0.8
 nPCs=20
 tiss2rem="cerebellum,cerebellar hemisphere"
 outTag="noCerebell"
 #rinFilt=6
-outDir="/home/users/gsantamaria/projects/brain_clock/results/preprocessing/integ_LINCSSamps_all_sva_fast/"
+outDir="/home/users/gsantamaria/projects/brain_clock/results/preprocessing/integ_LINCSSamps_wSC_all_sva_fast/"
 
 # Create output directory if it doesn't exist
 if [ ! -d "$outDir" ]; then
