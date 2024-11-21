2d9c2e0ee736d9fc53deb1271f860844c46809b8
Guillem Santamaria Aguilar guillem.santamaria@uni.lu <gsantamaria@access1.iris-cluster.uni.lux>
2024-11-21 18:10:35 +0100

    Edit NEAT script to accept external gene sets

--- scripts/func_enrich/NEAT_enrich.R
+++ scripts/func_enrich/NEAT_enrich.R
@@ -38,12 +38,19 @@ library(GO.db)
 parser <- arg_parser("NEAT functional erichment analyisis.")
 
 parser <- add_argument(parser = parser,
-                       arg = c("--DE", "--outName", "--FCFile", "--outDir"),
+                       arg = c("--DE",
+                               "--outName",
+                               "--extGeneSets",
+                               "--FCFile",
+                               "--FCVersion",
+                               "--outDir"),
                        help = c("Differentially expressed genes file",
                                 "Name given to the output file",
+                                "File to external gene sets object (RDS file consisting in a list, with the names being the names of the gene sets, and the elements the ENSEMBL IDs).",
                                 "Path to FunCoup file",
+                                "Version of the FunCoup file (5 or 6)",
                                 "Output directory to save the results."),
-                       flag = c(F, F, F, F))
+                       flag = c(F, F, F, F, F, F))
 
 parsed <- parse_args(parser)
 
@@ -66,15 +73,43 @@ addSlashIfNot <- function(pth){
         return(pth)
 }
 
+# Remove gene GO sets that don't have any gene included in the
+# FunCoup network
+filtGeneSets <- function(geneSets, network){
+        pb <- txtProgressBar(min = 0,
+                             max = length(names(geneSets)),
+                             initial = 0,
+                             width = 80,
+                             style = 3)
+        sets <- names(geneSets)
+        for(i in seq_along(sets)){
+                n <- sets[i]
+                setTxtProgressBar(pb, i)
+                set <- geneSets[[n]]
+                if(sum(set %in% unique(c(network$Gene1,
+                                         network$Gene2))) == 0){
+                        geneSets <- geneSets[names(geneSets) != n]
+                }
+        }
+        close(pb)
+        return(geneSets)
+}
+
 # Directory stuff
 ################################################################################
 
 inFile <- parsed$DE
 outName <- parsed$outName
+extGeneSets <- parsed$extGeneSets
 funCoupFile <- parsed$FCFile
+FCVersion <- as.numeric(parsed$FCVersion)
 outDir <- addSlashIfNot(parsed$outDir)
 
 createIfNot(outDir)
+
+if (extGeneSets == "NULL"){
+        extGeneSets <- NULL
+}
 
 outName <- paste0(outDir, outName)
 
@@ -88,63 +123,56 @@ DE_genes <- read.csv(inFile, row.names = 1)
 funCoup <- read.table(file = funCoupFile,
                       sep = "\t", header = T, comment.char = "")
 
+colnames(funCoup) <- gsub("X.0", "X0", colnames(funCoup))
+
 colnames(funCoup) <- sapply(colnames(funCoup),
                             function(x) strsplit(x,
                                                  ".",
                                                  fixed = T)[[1]][2])
 
-colnames(funCoup)[1] <- "PFC"
-
 # Filter the interactions to keep only high quality ones
-funCoup_filt <- funCoup[funCoup$PFC >= 0.75, ]
+if(FCVersion == 5){
+        funCoup_filt <- funCoup[funCoup$PFC >= 0.75, ]
+}else if (FCVersion == 6){
+        funCoup_filt <- funCoup[funCoup$PPV >= 0.75, ]
+}
 
 # Get gene sets for each GO biological process in H. sapiens
 
 # Create cache directory to store filtered GO gene sets
-cacheDir <- paste0(outDir, "cache/")
-if(!dir.exists(cacheDir)){
-        dir.create(cacheDir)
-}
-goGeneSets_file <- paste0(cacheDir, "goGeneSets.RData")
-if(!file.exists(paste0(goGeneSets_file))){
-        goGeneSets <- mapIds(org.Hs.eg.db,
-                             keys(org.Hs.eg.db, "GO"),
-                             "ENSEMBL",
-                             "GO",
-                             multiVals = "list")
-        # Remove gene GO sets that don't have any gene included in the
-        # FunCoup network
-        filtGeneSets <- function(geneSets, network){
-                pb <- txtProgressBar(min = 0,
-                                     max = length(names(geneSets)),
-                                     initial = 0,
-                                     width = 80,
-                                     style = 3)
-                sets <- names(geneSets)
-                for(i in seq_along(sets)){
-                        n <- sets[i]
-                        setTxtProgressBar(pb, i)
-                        set <- geneSets[[n]]
-                        if(sum(set %in% unique(c(network$Gene1,
-                                                 network$Gene2))) == 0){
-                                geneSets <- geneSets[names(geneSets) != n]
-                        }
-                }
-                close(pb)
-                return(geneSets)
+if(is.null(extGeneSets)){
+        cacheDir <- paste0(outDir, "cache/")
+        if(!dir.exists(cacheDir)){
+                dir.create(cacheDir)
         }
-        
-        print("Filtering gene GO sets to keep just the ones that are
-              represented in FunCoup network...")
-        goGeneSets_filtered <- filtGeneSets(goGeneSets, funCoup_filt)
-        print(sprintf("Done, goGeneSets.RData saved at %s.", cacheDir))
-        save(goGeneSets_filtered, file = paste0(cacheDir, "goGeneSets.RData"))
+        goGeneSets_file <- paste0(cacheDir, "goGeneSets.RData")
+        if(!file.exists(paste0(goGeneSets_file))){
+                goGeneSets <- mapIds(org.Hs.eg.db,
+                                  keys(org.Hs.eg.db, "GO"),
+                                   "ENSEMBL",
+                                     "GO",
+                                     multiVals = "list")
+                # Remove gene GO sets that don't have any gene included in the
+                # FunCoup network
+                print("Filtering gene GO sets to keep just the ones that are
+                      represented in FunCoup network...")
+                goGeneSets_filtered <- filtGeneSets(goGeneSets, funCoup_filt)
+                print(sprintf("Done, goGeneSets.RData saved at %s.", cacheDir))
+                save(goGeneSets_filtered, file = paste0(cacheDir, "goGeneSets.RData"))
+        }else{
+                print(sprintf("Filtered gene GO sets already exists. Loading
+                goGeneSets.RData from %s", cacheDir))
+                load(file = paste0(cacheDir, "goGeneSets.RData"))
+        }
 }else{
-        print(sprintf("Filtered gene GO sets already exists. Loading
-        goGeneSets.RData from %s", cacheDir))
-        load(file = paste0(cacheDir, "goGeneSets.RData"))
+        print(sprintf("Loading external gene sets from %s, at %s.",
+                      basename(extGeneSets),
+                      dirname(extGeneSets)))
+        goGeneSets <- readRDS(extGeneSets)
+        print("Filtering gene sets to keep just the ones that are
+                      represented in FunCoup network...")
+        goGeneSets_filtered <- filtGeneSets(goGeneSets, funCoup_filt)
 }
-
 
 # Create NEAT target genes list, converting symbols to ENSEMBL IDs and
 # removing any gene that is not included in the FunCoup interaction file.
--- scripts/func_enrich/launch_NEAT_enrich_mice_DEGs_all.sh
+++ scripts/func_enrich/launch_NEAT_enrich_mice_DEGs_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #Usage # sbatch [this script]
 #Name of the job
-#SBATCH --job-name=NEAT_enr_signChrn
+#SBATCH --job-name=NEAT_enr_mDEGsAll
 #SBATCH -N 1
 #SBATCH --mail-user=guillem.santamaria@uni.lu
 #SBATCH --mail-type=begin,end,fail
@@ -10,9 +10,9 @@
 #SBATCH -c 1
 #SBATCH --time=00-03:00:00
 #Define sdout path
-#SBATCH --output=/home/users/gsantamaria/projects/brain_clock/scripts/func_enrich/output_NEAT_enrich_signChrn.txt
+#SBATCH --output=/home/users/gsantamaria/projects/brain_clock/scripts/func_enrich/output_NEAT_enrich_mDEGsAll.txt
 #Define sderr path
-#SBATCH --error=/home/users/gsantamaria/projects/brain_clock/scripts/func_enrich/error_NEAT_enrich_signChrn.txt
+#SBATCH --error=/home/users/gsantamaria/projects/brain_clock/scripts/func_enrich/error_NEAT_enrich_mDEGsAll.txt
 #Define the queue (Quality Of Service) to which the task shall be submitted to
 #SBATCH -p batch
 #SBATCH --qos=normal
@@ -25,13 +25,13 @@ conda activate r-4.3.1
 
 # Variables for the pipeline
 ########################################################################################################################
-DE_file="/home/users/gsantamaria/projects/brain_clock/results/models/modAllGenes_ingegWAllLincsBrain_and_sc_sva_chron_age_onSignGenes/modFuncsAlpha1_coefs.csv"
-outNameNEAT="mod_all_chronSign_neat.csv"
-extGeneSets=NULL
+DE_file="/home/users/gsantamaria/projects/brain_clock/results/mice_experiments/DEGs_all.csv"
+outNameNEAT="mice_DEGs_all_neat.csv"
+extGeneSets="/home/users/gsantamaria/projects/brain_clock/data/geneSets_tMuris.rds"
 FCVersion=5
 conv2ENSEMBL=false
-FCFile="~/projects/cureMILS/proteomics/data/FunCoup/FC5.0_H.sapiens_full"
-outDir="/home/users/gsantamaria/projects/brain_clock/results/enrich_NEAT/mod_all_signChron_431signChronGenes/"
+FCFile="/home/users/gsantamaria/projects/cureMILS/proteomics/data/FunCoup/FC5.0_M.musculus_full.gz"
+outDir="/home/users/gsantamaria/projects/brain_clock/results/enrich_NEAT/mice_exps/"
 
 # If the datasets and dataformat are not installed this script will install them
 bash install_ncbi_CLTools.sh
