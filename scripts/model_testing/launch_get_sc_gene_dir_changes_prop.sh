ad8bc27df6959f371d91691c5e8e891c0b5f1e42
Guillem Santamaria Aguilar guillem.santamaria@uni.lu <gsantamaria@access1.iris-cluster.uni.lux>
2025-02-05 17:01:11 +0100

    Fix bugs

--- scripts/model_testing/launch_get_sc_gene_dir_changes_prop.sh
+++ scripts/model_testing/launch_get_sc_gene_dir_changes_prop.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=100GB
 #SBATCH -c 8
-#SBATCH --time=00-00:40:00
+#SBATCH --time=00-12:00:00
 #Define sdout path
 #SBATCH --output=/home/users/gsantamaria/projects/brain_clock/scripts/model_testing/output_scGeneDirChngs.txt
 #Define sderr path
